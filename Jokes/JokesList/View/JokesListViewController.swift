4b7f01ca30c93f0ce2368feca3e7c08c249defef
Ashish <a.maheshwari9855@gmail.com>
2018-05-07 12:00:25 +0200

    Write datasource methods of tableview to class

--- Jokes/JokesList/View/JokesListViewController.swift
+++ Jokes/JokesList/View/JokesListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class JokesListViewController: UIViewController {
 
+    let cellIndetifier = "JokeTableViewCell"
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var firstNameField: UITextField!
@@ -24,5 +26,19 @@ class JokesListViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+}
 
+extension JokesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIndetifier, for: indexPath)
+        return cell
+    }
 }
