d75a0a0181a0b2debd61477fea6f2411f56508b3
Ashish <a.maheshwari9855@gmail.com>
2018-05-07 12:09:02 +0200

    Create Protocols
    # This commit create protocols fro presenter, interactor, view

--- Jokes/JokesList/JokesListViewControllerProtocol.swift
+++ Jokes/JokesList/JokesListViewControllerProtocol.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import UIKit
+
+protocol JokesListViewControllerInteractorInputProtocol: class {
+    
+    var presenter: JokesListViewControllerInteractorOutputProtocol? { get set }
+    var network: Network? { get set }
+    func retrieveJokes()
+}
+
+protocol JokesListViewControllerInteractorOutputProtocol: class {
+    
+    // INTERACTOR -> PRESENTER
+    func didRetrieve(jokes: [Joke])
+    func onError(with: APIError)
+}
+
+protocol JokesListViewControllerPresenterProtocol: class {
+    
+    var view: JokesListViewControllerProtocol? { get set }
+    var wireFrame: JokesListViewControllerWireFrameProtocol? { get set }
+    var interactor: JokesListViewControllerInteractorInputProtocol? { get set }
+    var jokes:[Joke] {get set}
+    
+    // VIEW -> PRESENTER
+    func didLoad()
+}
+
+protocol JokesListViewControllerWireFrameProtocol: class {
+    static func createJokesListModule() -> UIViewController
+}
+
+protocol JokesListViewControllerProtocol: class {
+    
+    var presenter: JokesListViewControllerPresenterProtocol? { get set }
+    
+    // PRESENTER -> VIEW
+    func showJokes()
+    func showLoading()
+    func hideLoading()
+}
