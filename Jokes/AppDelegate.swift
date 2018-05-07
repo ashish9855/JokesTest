539e1b925a5380d526386ee9f8e64bcda4790e26
Ashish <a.maheshwari9855@gmail.com>
2018-05-07 21:30:20 +0200

    Instantiate First controller from a--delegate

--- Jokes/AppDelegate.swift
+++ Jokes/AppDelegate.swift
@@ -12,10 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var jokesView: UIViewController?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        jokesView = JokesListViewControllerWireframe.createJokesListModule()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = jokesView
+        window?.makeKeyAndVisible()
         return true
     }
 
