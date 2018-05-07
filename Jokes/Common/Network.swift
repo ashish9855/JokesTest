3fb1e624e44822652b8069b8b5ecea4d114be8fc
Ashish <a.maheshwari9855@gmail.com>
2018-05-07 11:40:30 +0200

    Create GET request method
    # This method will take Url , then form request and return json

--- Jokes/Common/Network.swift
+++ Jokes/Common/Network.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 protocol NetworkProtocols: class {
-    func onSuccess(_ data: [Joke])
+    func onSuccess(_ data: AnyObject)
     func onError(_ error: APIError)
 }
 
@@ -23,5 +23,30 @@ class Network {
     
     init(_ delegate: NetworkProtocols?) {
         self.delegate = delegate
+    }
+    
+    func makeRequest(with url: URL, method: HTTPMethod, parameters: [AnyHashable : Any]?) {
+        
+        let session = URLSession(configuration: .default)
+        let request = URLRequest(url: url)
+        let task = session.dataTask(with: request) {[weak self] (data, response, error) in
+            
+            if (error != nil) {
+                let apiError = APIError.ServerError(message: (error?.localizedDescription)!)
+                self?.delegate?.onError(apiError)
+                return
+            }
+            else {
+                if let data = data {
+                    guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                        let apiError = APIError.ServerError(message: "Some error occured")
+                        self?.delegate?.onError(apiError)
+                        return
+                    }
+                    self?.delegate?.onSuccess(json as AnyObject)
+                }
+            }
+        }
+        task.resume()
     }
 }
